c2edf82406f174953a1d4dfb14b0fed36a2361e5
Jose Hernandez <puch007@mail.fresnostate.edu>
2019-10-30 23:58:52 -0700

    what i changed

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -8,6 +8,7 @@ course: string;
 editMode: boolean;
 }
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'main-content',
   templateUrl: './main-content.component.html',
   styleUrls: ['./main-content.component.css']
